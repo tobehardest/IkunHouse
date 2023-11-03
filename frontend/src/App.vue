7d6c7d20c6f1a5e2b56a3120cfe3be91f644c056
chenchen <2535705423@qq.com>
2023-11-03 22:54:42 +0800

    前端项目构建

---
+++ frontend/src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+//import useUserStore from '@/store/modules/user'
+
+//const userStore = useUserStore()
+</script>
+
+<template>
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
